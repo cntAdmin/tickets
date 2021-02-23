ed4231edbaa33d3f6668fc0b6679173dc129e4bd
cntAdmin <nacho@costanetworks.es>
2021-02-23 16:47:59 +0100

    minor

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::before(function ($user) {
-            return $user->hasRole('superadmin') ? true : null;
+            return $user->hasRole('superadmin') ? true : false;
         });
     }
 }
