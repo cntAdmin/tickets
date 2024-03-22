6ab20ad782088ed383ac8c5fe7d5c33de7b2981e
Matias <matias@costanetworks.es>
2024-03-22 13:25:14 +0100

    Añadir comentario en ticket indicando que pasa a estado resuelto

--- app/Http/Controllers/TicketStatusController.php
+++ app/Http/Controllers/TicketStatusController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Ticket;
-use App\Models\TicketStatus;
+use Str;
 use Carbon\Carbon;
+use App\Models\Ticket;
+use App\Models\Comment;
+use App\Models\TicketStatus;
 use Illuminate\Http\Request;
 
 class TicketStatusController extends Controller
@@ -16,14 +18,22 @@ class TicketStatusController extends Controller
 
     public function change_status(Ticket $ticket, TicketStatus $ticketStatus) 
     {
+        // Estado => Resuelto
         if($ticketStatus->id === 4) {
-            // $ticket->update(['answered' => 1]);
             $ticket_created_at = Carbon::parse($ticket->created_at);
             $diff_in_minutes = $ticket_created_at->diffInMinutes(Carbon::now());
 
             $ticket->update([
                 'answered' => 1,
                 'resolution_time' => $diff_in_minutes
+            ]);
+
+            // creamos un comentario informando que el ticket se ha resuelto
+            Comment::create([
+                'ticket_id' => $ticket->id,
+                'user_id' => auth()->user()->id ?? $ticket->user->id,
+                'description' => 'Ticket Resuelto.',
+                '_token' => $this->createToken(),
             ]);
         }
         $ticket->status()->associate($ticketStatus);
@@ -33,5 +43,17 @@ class TicketStatusController extends Controller
             'success' => true,
             'ticket' => $ticket
         ]);
+    }
+
+    public function createToken()
+    {
+        $token = Str::uuid();
+        $exists = Comment::where('_token', $token)->first();
+
+        if(!$exists) {
+            return $token;
+        } else {
+            $this->createToken();
+        }
     }
 }
